1dfe523a7b0efa04efda7b047359a6c8bfe90ef7
moosejaw <joshdemir@gmail.com>
2019-10-07 19:34:43 +0100

    Initial commit of tshark cmd script

---
+++ tshark.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+
+TSHARK_COMMANDS = ['tshark', '-c', '7500']
+TSHARK_OUTPUT_FILE = '~/tshark-output.txt'
+
+if __name__ == '__main__':
+    with open(TSHARK_OUTPUT_FILE, 'w') as f:
+        f.write(subprocess.check_output(TSHARK_COMMANDS, \
+                shell=True,
+                stderr=subprocess.STDOUT)
+
+

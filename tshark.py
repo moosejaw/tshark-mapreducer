f42c4c6882e64bc8990740c85a2257246e7f5e10
moosejaw <joshdemir@gmail.com>
2019-10-07 20:15:45 +0100

    Use Popen and read from stdout

--- tshark.py
+++ tshark.py
@@ -1,11 +1,16 @@
 import subprocess
 import sys
 
-TSHARK_COMMANDS = ['tshark', '-c', '7500']
+TSHARK_NUMBER_OF_PACKETS = 10
+TSHARK_COMMANDS = ['tshark', '-c', str(TSHARK_NUMBER_OF_PACKETS)]
 TSHARK_OUTPUT_FILE = '/home/hduser/tshark-output.txt'
 
 if __name__ == '__main__':
+    print(f'Running tshark and getting {TSHARK_NUMBER_OF_PACKETS} packets...')
+
+    # Write tshark stdout to output file
     with open(TSHARK_OUTPUT_FILE, 'w') as f:
-        f.write(subprocess.check_output(TSHARK_COMMANDS,
-                shell=True,
-                stderr=subprocess.STDOUT))
+        proc = subprocess.Popen(TSHARK_COMMANDS,
+                stdout=subprocess.PIPE)
+        proc.communicate()
+        f.write(proc.stdout.read())

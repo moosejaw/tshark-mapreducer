94698108991761a5ea51f4a5f81126981abff516
moosejaw <joshdemir@gmail.com>
2019-10-07 19:37:33 +0100

    Fix typo in f.write()

--- tshark.py
+++ tshark.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     with open(TSHARK_OUTPUT_FILE, 'w') as f:
         f.write(subprocess.check_output(TSHARK_COMMANDS,
                 shell=True,
-                stderr=subprocess.STDOUT)
+                stderr=subprocess.STDOUT))
